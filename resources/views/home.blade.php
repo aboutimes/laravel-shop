d4dae66e5f44a80e915c1abe59cd66670402ff75
aboutimes <aboutime2012@gmail.com>
2018-06-15 19:59:35 +0800

    fix footer style

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1 style="text-align: center">这是首页</h1>
+    <div class="main-content">
+        <h1 style="text-align: center">这是首页</h1>
+    </div>
 @endsection
